58f526e73079205540cd68b9896792ebf380d899
Esther Arias <eariasvalor@gmail.com>
2025-12-17 11:23:54 +0100

    feat: add PlayerRepository interface (domain port)

---
+++ src/main/java/com/blackjack/domain/repository/PlayerRepository.java
@@ -0,0 +1,18 @@
+package com.blackjack.domain.repository;
+
+import com.blackjack.domain.model.aggregate.Player;
+import com.blackjack.domain.model.valueobject.player.PlayerId;
+import com.blackjack.domain.model.valueobject.player.PlayerName;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PlayerRepository {
+    Mono<Player> save(Player player);
+    Mono<Player> findById(PlayerId id);
+    Mono<Player> findByName(PlayerName name);
+    Flux<Player> findAll();
+    Flux<Player> findTopByWinRate(int limit);
+    Mono<Boolean> existsByName(PlayerName name);
+    Mono<Boolean> existsById(PlayerId id);
+    Mono<Long> count();
+}
