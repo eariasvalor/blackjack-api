c847784a3de55d7e19ac98d7b4b8b83be43e3d74
eariasvalor <eariasvalor@gmail.com>
2025-12-29 23:16:21 +0100

    feat: add health controller

--- src/main/java/com/blackjack/infrastructure/web/controller/HealthController.java
+++ src/main/java/com/blackjack/infrastructure/web/controller/HealthController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/health") // La URL será /health (más simple que /actuator/health)
+@RequestMapping("/health")
 @Tag(name = "Health Check", description = "Endpoint for verifying the API status")
 public class HealthController {
 
