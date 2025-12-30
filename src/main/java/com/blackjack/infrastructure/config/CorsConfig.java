1aadbe71dcd45bc4c6d0545897ca58a8ec5d5cc5
eariasvalor <eariasvalor@gmail.com>
2025-12-30 22:14:32 +0100

    fix: use corswebfilter method

--- src/main/java/com/blackjack/infrastructure/config/CorsConfig.java
+++ src/main/java/com/blackjack/infrastructure/config/CorsConfig.java
@@ -2,23 +2,29 @@ package com.blackjack.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(Collections.singletonList("*"));
+
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowedHeaders(Collections.singletonList("*"));
+
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
